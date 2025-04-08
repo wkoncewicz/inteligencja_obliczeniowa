3a3938d700f6945e1255901f116bb0ebc618dfe6
Wiktor Koncewicz <w.koncewicz.189@studms.ug.edu.pl>
2025-04-08 22:10:21 +0200

    lab5 zad3 almost

--- lab5/zad2.py
+++ lab5/zad2.py
@@ -7,6 +7,7 @@ from tensorflow.keras.layers import Dense, Conv2D, Flatten, MaxPooling2D
 from tensorflow.keras.utils import to_categorical
 from sklearn.metrics import confusion_matrix
 from tensorflow.keras.callbacks import History
+from tensorflow.keras.callbacks import ModelCheckpoint
 
 # Load dataset
 (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
@@ -17,6 +18,7 @@ test_images = test_images.reshape((test_images.shape[0], 28, 28, 1)).astype('flo
 train_labels = to_categorical(train_labels)
 test_labels = to_categorical(test_labels)
 original_test_labels = np.argmax(test_labels, axis=1) # Save original labels for confusion matrix
+print("oryginał", original_test_labels)
 
 # Define model
 model = Sequential([
@@ -30,10 +32,11 @@ model = Sequential([
 # Compile model
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
+checkpoint = ModelCheckpoint('best_model.h5', monitor='val_accuracy', mode='max', save_best_only=True, verbose=1)
 # Train model
 history = History()
 model.fit(train_images, train_labels, epochs=5, batch_size=64, validation_split=0.2,
-callbacks=[history])
+callbacks=[checkpoint, history])
 
 # Evaluate on test set
 test_loss, test_acc = model.evaluate(test_images, test_labels)
@@ -84,4 +87,18 @@ for i in range(25):
     plt.xlabel(predicted_labels[i])
 plt.show()
 
-#a)
+#a) reshape dodaje kanał głębokości, żeby dane miały taki format jakiego oczekuje warstwa Conv2D; (60000, 28, 28) → (60000, 28, 28, 1)
+#to_categorical - oryginalne etykiety to liczby 0–9, to_categorical zamienia je na one-hot encoding
+#argmax - zwraca index największego elementu, czyli przywraca one-hot do normalnych danych
+
+#b) Conv2D - (32, (3, 3)) dla (28, 28, 1) -> (28 - 3 + 1, 28 - 3 + 1, 32) -> (26, 26, 32)
+#MaxPooling2D - (2, 2) dla (26, 26, 32) -> dzieli na 2x2 bloki -> (13, 13, 32)
+#Flatten() -> spłaszcza do 1D -> (13, 13, 32) -> 13 * 13 * 32 -> 5408
+#Dense(64, activation='relu') -> 64 neuronów wyjściowych -> 64 neurony z różnymi cechami określającymi cyfry
+#Dense(10, activation='softmax') -> warstwa klasyfikacyjna, połączenie 64 neuronów cech cyfr w 10 neuronów za każdą liczbę od 0 do 9
+
+#c) najczęściej mylona cyfra 5 - z 8, 6 i 3. najczęściej mylone z sobą cyfry to 7 z 2
+
+#d) pod koniec dokładność walidacyjna przestaje rosnąć co może sugerować przeuczenie
+
+#e) z dodaniem checkpoint do modelu 2 nie jest tak często mylona z 7 ale 3 z 5 jest częściej mylona
---
+++ lab5/zad3.py
@@ -0,0 +1,30 @@
+import numpy as np
+import pandas as pd
+from tensorflow.keras.preprocessing.image import ImageDataGenerator, load_img
+from tensorflow.keras.utils import to_categorical
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+import random
+import os
+
+FAST_RUN = False
+IMAGE_WIDTH=128
+IMAGE_HEIGHT=128
+IMAGE_SIZE=(IMAGE_WIDTH, IMAGE_HEIGHT)
+IMAGE_CHANNELS=3
+
+filenames = os.listdir("./dogs-cats-mini")
+categories = []
+for filename in filenames:
+    category = filename.split('.')[0]
+    if category == 'dog':
+        categories.append(1)
+    else:
+        categories.append(0)
+
+df = pd.DataFrame({
+    'filename': filenames,
+    'category': categories
+})
+
+
