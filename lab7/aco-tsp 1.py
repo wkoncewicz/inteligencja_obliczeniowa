481a6d53c0d52aad61296a8e6292fd0c7e08c4e3
Wiktor Koncewicz <w.koncewicz.189@studms.ug.edu.pl>
2025-04-22 21:35:01 +0200

    lab6 and 8 update

--- lab7/aco-tsp 1.py
+++ lab7/aco-tsp 1.py
@@ -1,11 +1,10 @@
 import matplotlib.pyplot as plt
 import random
-
 from aco import AntColony
+import time
 
-
+start = time.time()
 plt.style.use("dark_background")
-
 
 COORDS = (
     (20, 52),
@@ -40,11 +39,15 @@ def plot_all_edges():
 
 plot_nodes()
 
-colony = AntColony(COORDS, ant_count=300, alpha=0.5, beta=1.2, 
-                    pheromone_evaporation_rate=0.40, pheromone_constant=1000.0,
-                    iterations=300)
+colony = AntColony(COORDS, ant_count=300, alpha=0.5, beta=1.2,
+                pheromone_evaporation_rate=0.40, pheromone_constant=1000.0,
+                iterations=300)
 
 optimal_nodes = colony.get_path()
+
+end = time.time()
+duration = end - start
+print("time:", duration)
 
 for i in range(len(optimal_nodes) - 1):
     plt.plot(
@@ -54,3 +57,18 @@ for i in range(len(optimal_nodes) - 1):
 
 
 plt.show()
+
+#default - 240.6556715522927
+#czas: 18.04226565361023
+
+#colony = AntColony(COORDS, ant_count=150, alpha=1.0, beta=3.0,
+                    # pheromone_evaporation_rate=0.20, pheromone_constant=500.0,
+                    # iterations=200)
+#231.5510487538921 / 240.65567155229272
+#czas: 6.019184112548828
+
+#colony = AntColony(COORDS, ant_count=500, alpha=2.0, beta=2.0,
+                    # pheromone_evaporation_rate=0.60, pheromone_constant=1500.0,
+                    # iterations=100)
+#240.65567155229272 / 231.5510487538921
+#czas: 10.097235202789307
---
+++ lab8/zad1.py
@@ -0,0 +1,54 @@
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from collections import Counter
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+
+nltk.download('punkt_tab')
+nltk.download('stopwords')
+nltk.download('wordnet')
+nltk.download('omw-1.4')
+
+with open("article.txt", "r", encoding="utf-8") as file:
+    text = file.read()
+
+tokens = word_tokenize(text)
+print("Liczba tokenów:", len(tokens))
+
+words = [word for word in tokens if word.isalpha()]
+stop_words = set(stopwords.words('english'))
+filtered_words = [word for word in words if word.lower() not in stop_words]
+print("Liczba słów po usunięciu stop-words:", len(filtered_words))
+
+my_stop_words = {'also', 'data'}
+stop_words.update(my_stop_words)
+filtered_words = [word for word in words if word.lower() not in stop_words]
+print("Liczba słów po usunięciu moich stop-words:", len(filtered_words))
+
+lemmatizer = WordNetLemmatizer()
+lemmatized_words = [lemmatizer.lemmatize(word.lower()) for word in filtered_words]
+print("Liczba słów po lematyzacji:", len(lemmatized_words))
+
+word_freq = Counter(lemmatized_words)
+most_common = word_freq.most_common(10)
+words_x, counts_y = zip(*most_common)
+
+plt.figure(figsize=(10, 6))
+plt.bar(words_x, counts_y, color='skyblue')
+plt.xlabel("Słowa")
+plt.ylabel("Liczba wystąpień")
+plt.title("10 najczęściej występujących słów w artykule")
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.show()
+
+text_for_wordcloud = ' '.join(filtered_words)
+wordcloud = WordCloud(width=800, height=400, background_color='white').generate(text_for_wordcloud)
+
+plt.figure(figsize=(12, 6))
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis('off')
+plt.title("Chmura tagów dla artykułu")
+plt.show()
