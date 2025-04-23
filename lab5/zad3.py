9c4dc44a7aca87a890fdd85301fe803139138679
Wiktor Koncewicz <wkoncewicz@INF.UG.EDU.PL>
2025-04-23 11:10:11 +0200

    model update

--- lab5/zad3.py
+++ lab5/zad3.py
@@ -30,28 +30,39 @@ df = pd.DataFrame({
     'category': categories
 })
 
-model = Sequential()
+# model = Sequential()
+#
+# model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_CHANNELS)))
+# model.add(BatchNormalization())
+# model.add(MaxPooling2D(pool_size=(2, 2)))
+# model.add(Dropout(0.25))
+#
+# model.add(Conv2D(64, (3, 3), activation='relu'))
+# model.add(BatchNormalization())
+# model.add(MaxPooling2D(pool_size=(2, 2)))
+# model.add(Dropout(0.25))
+#
+# model.add(Conv2D(128, (3, 3), activation='relu'))
+# model.add(BatchNormalization())
+# model.add(MaxPooling2D(pool_size=(2, 2)))
+# model.add(Dropout(0.25))
+#
+# model.add(Flatten())
+# model.add(Dense(512, activation='relu'))
+# model.add(BatchNormalization())
+# model.add(Dropout(0.5))
+# model.add(Dense(2, activation='softmax')) # 2 because we have cat and dog classes
 
-model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_CHANNELS)))
-model.add(BatchNormalization())
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(BatchNormalization())
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-
-model.add(Conv2D(128, (3, 3), activation='relu'))
-model.add(BatchNormalization())
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-
-model.add(Flatten())
-model.add(Dense(512, activation='relu'))
-model.add(BatchNormalization())
-model.add(Dropout(0.5))
-model.add(Dense(2, activation='softmax')) # 2 because we have cat and dog classes
+model = Sequential([
+    Conv2D(32, (3,3), activation='relu', input_shape=(128,128,3)),
+    MaxPooling2D(2,2),
+    Conv2D(64, (3,3), activation='relu'),
+    MaxPooling2D(2,2),
+    Flatten(),
+    Dense(128, activation='relu'),
+    Dropout(0.5),
+    Dense(1, activation='sigmoid')
+])
 
 model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
 
@@ -137,4 +148,4 @@ legend = plt.legend(loc='best', shadow=True)
 plt.tight_layout()
 plt.show()
 
-model.save("model.h5")
+model.save("model2.h5")
