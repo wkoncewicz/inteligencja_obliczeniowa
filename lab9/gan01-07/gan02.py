17607b3631692c041a53335a0ce7b077cf3341bb
Wiktor Koncewicz <w.koncewicz.189@studms.ug.edu.pl>
2025-05-28 11:19:09 +0200

    lab9

---
+++ lab9/gan01-07/gan02.py
@@ -0,0 +1,30 @@
+# example of defining the discriminator model
+from keras.models import Sequential # type: ignore
+from keras.optimizers import Adam # type: ignore
+from keras.layers import Dense, Conv2D, Flatten, Dropout, LeakyReLU # type: ignore
+from keras.utils import plot_model # type: ignore
+import os
+os.environ["PATH"] += os.pathsep + r'C:\Program Files\Graphviz\bin'
+
+# define the standalone discriminator model
+def define_discriminator(in_shape=(28,28,1)):
+	model = Sequential()
+	model.add(Conv2D(64, (3,3), strides=(2, 2), padding='same', input_shape=in_shape))
+	model.add(LeakyReLU(alpha=0.2))
+	model.add(Dropout(0.4))
+	model.add(Conv2D(64, (3,3), strides=(2, 2), padding='same'))
+	model.add(LeakyReLU(alpha=0.2))
+	model.add(Dropout(0.4))
+	model.add(Flatten())
+	model.add(Dense(1, activation='sigmoid'))
+	# compile model
+	opt = Adam(learning_rate=0.0002, beta_1=0.5)
+	model.compile(loss='binary_crossentropy', optimizer=opt, metrics=['accuracy'])
+	return model
+
+# define model
+model = define_discriminator()
+# summarize the model
+model.summary()
+# plot the model
+plot_model(model, to_file='discriminator_plot.png', show_shapes=True, show_layer_names=True)
